028753fc9d8f0432b03079688b44b8d6f7e54b8d
Kieron Scott <hello@greatscott.design>
2024-03-11 18:42:00 +0000

    Add project files

---
+++ scripts.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const list = document.querySelector("#myList");
+  const form = document.querySelector("#myForm");
+  const dbURL = "http://localhost:3000/list";
+
+  function getList() {
+    list.innerHTML = "";
+    fetch(`${dbURL}`)
+      .then((response) => response.json())
+      .then((listData) => {
+        if (listData.length > 0) {
+          listData.forEach(function (item) {
+            list.innerHTML += `
+        <li id=${item.id}>
+          ${item.title}
+          <button data-id="${item.id}" id="edit-${item.id}" data-action="edit">Edit</button>
+          <button data-id="${item.id}" id="delete-${item.id}" data-action="delete">Delete</button>
+        </li>`;
+          });
+        } else {
+          list.innerHTML = `<li>Nothing on the list yet...</li>`;
+        }
+      });
+  }
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    function addItem() {
+      const input = document.querySelector("#myInput");
+      fetch(`${dbURL}`, {
+        method: "POST",
+        body: JSON.stringify({
+          title: input.value,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      input.value = "";
+    }
+    addItem();
+  });
+
+  getList();
+});
